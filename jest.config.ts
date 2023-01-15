e84d395010979202a39186b9b42a0344894860e1
baturalpkaramus <baturalpkaramus@gmail.com>
2023-01-15 15:26:16 +0300

    jest config

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+
+  clearMocks: true,
+
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  // Test spec file resolution pattern
+  // Matches parent folder `__tests__` and filename
+  // should contain `test` or `spec`.
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+
+  // Module file extensions for importing
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+  }
+};
